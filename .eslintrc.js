e2c2144969118843ecce4ef9895518b8f809ed7e
Dea9703 <dea9703@163.com>
2023-04-08 13:34:51 +0800

    手搭 webpack5 + vue3 框架

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // 环境设置，环境里面没有的不能使用
+  env: {
+    browser: true,
+    es2021: true
+  },
+  // // 继承，可以在这里继承那些配置好的规范，不再需要一条条的去手写
+  extends: [
+    // 继承的插件（需要在 plugins 中注册）
+    'plugin:vue/strongly-recommended'
+  ],
+  // 插件，额外的 rules + 提供一套现成的规范
+  plugins: [
+    'vue'
+  ],
+  parserOptions: {
+    ecmaVersion: 6, // 指定使用的 es 版本
+    sourceType: 'module', // 模块化
+    ecmaFeature: { // 额外语言特性
+      jsx: true
+    }
+  },
+  // 在这里编写eslint校验规则，会覆盖 extends 里面继承的规则
+  rules: {
+    /**
+    * 0 or off：关闭检查
+    * 1 or warn: 检查警告
+    * 2 or error：检查报错
+    */
+    'semi': [ 'error', 'never' ],
+    'quotes': [ 'error', 'single' ],
+    'no-mixed-spaces-and-tabs': 2,
+    // 不是2个空格缩进报错
+    'indent': [ 'error', 2 ],
+    'no-multi-spaces': 2,
+    'key-spacing': 2,
+    'object-curly-spacing': [ 2, 'always' ],
+    'array-bracket-spacing': [ 2, 'always' ],
+    'comma-spacing': [ 2, { 'before': false, 'after': true } ],
+    'function-paren-newline': [ 'error', { 'minItems': 5 } ],
+    'no-empty-function': 'error',
+    'keyword-spacing': [ 'error', { 'after': true, } ],
+    'space-before-blocks': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': [
+      2, {
+        'words': true,
+        'nonwords': false,
+      } ],
+    'spaced-comment': [ 'error', 'always' ],
+    'semi-spacing': [ 'error', { 'before': false, 'after': true } ],
+    'arrow-body-style': [ 'error', 'as-needed' ],
+    'arrow-parens': [ 'error', 'as-needed' ],
+    'arrow-spacing': 'error',
+    'vue/comment-directive': 'off'
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const EsLintPlugin = require('eslint-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const _resolve = src => path.resolve(__dirname, src)
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: _resolve('src/main.js')
+  },
+  output: {
+    path: _resolve('dist'),
+    filename: '[name][hash:5].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: [
+                    '>1%', // 占有率大于 1% 的浏览器
+                    'last 2 versions', // 浏览器最新的两个版本 
+                    'not ie <= 8' // 不支持 ie8 及以下的浏览器
+                  ]
+                }
+              }
+            ] ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        // use 后面跟数组，表示这个文件由多个 loader 去处理，处理顺序是由后到前
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // 消除浏览器控制台工具警告
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+    new HTMLWebpackPlugin({
+      template: _resolve('index.html'), // 指定要使用的 html 模板地址
+      filename: 'index.html', // 打包后输出的文件名
+      title: '手搭 vue3 开发环境' // index.html 模板内，通过 <%= htmlWebpackPlugin.options.title %> 拿到变量
+    }),
+    new VueLoaderPlugin(),
+    new EsLintPlugin(),
+    // 打包时清除旧的打包，只保留当前最新的打包
+    new CleanWebpackPlugin()
+  ]
+}
